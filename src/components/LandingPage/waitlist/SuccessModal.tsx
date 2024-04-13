58056349ed591c2f3c461e64c607b1e5111517ce
ajricks4 <ajricks4@gmail.com>
2024-04-13 13:47:50 -0700

    Add indicator for clipboard has updated with link

--- src/components/LandingPage/waitlist/SuccessModal.tsx
+++ src/components/LandingPage/waitlist/SuccessModal.tsx
@@ -41,6 +41,7 @@ export default function SuccessModal({
   const [points, setPoints] = useState(0);
   const [reloadPoints, setReloadPoints] = useState(false);
   const [waitList, setWaitList] = useState<Waitlist | null>(null);
+  const [copied, setCopied] = useState(false);
   const {
     control,
     handleSubmit,
@@ -120,6 +121,13 @@ export default function SuccessModal({
     }
     loadPoints();
   }, [submitted, email, apiSecretKey, reloadPoints]);
+  const handleCopy = () => {
+    navigator.clipboard.writeText(
+      `https://www.yourhomebase.co?ref=${referralCode}`,
+    );
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+  };
   return (
     <Transition.Root show={showSuccess} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={setShowSuccess}>
@@ -186,22 +194,37 @@ export default function SuccessModal({
                                   value={`https://www.yourhomebase.co?ref=${referralCode}`}
                                 />
                                 <button
-                                  className="absolute bottom-0 right-0 top-0 rounded-r-md bg-primary px-4 text-white transition duration-200 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                  onClick={() => {
-                                    navigator.clipboard.writeText(
-                                      `https://www.yourhomebase.co?ref=${referralCode}`,
-                                    );
-                                  }}
+                                  className={`absolute bottom-0 right-0 top-0 rounded-r-md px-4 text-white transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                                    copied
+                                      ? "bg-green-500 hover:bg-green-600"
+                                      : "bg-primary hover:bg-blue-600"
+                                  }`}
+                                  onClick={handleCopy}
                                 >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-5 w-5"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                  >
-                                    <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
-                                    <path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z" />
-                                  </svg>
+                                  {copied ? (
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      className="h-5 w-5"
+                                      viewBox="0 0 20 20"
+                                      fill="currentColor"
+                                    >
+                                      <path
+                                        fillRule="evenodd"
+                                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                        clipRule="evenodd"
+                                      />
+                                    </svg>
+                                  ) : (
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      className="h-5 w-5"
+                                      viewBox="0 0 20 20"
+                                      fill="currentColor"
+                                    >
+                                      <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
+                                      <path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z" />
+                                    </svg>
+                                  )}
                                 </button>
                               </div>
                             </div>
