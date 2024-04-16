7cffc4d2963a39c51b93f1e3e03a94ac4aae486d
ajricks4 <ajricks4@gmail.com>
2024-04-15 23:58:58 -0700

    magic link email authentication

---
+++ src/components/Authentication/SignIn.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { signIn } from "@/auth";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Mail } from "lucide-react";
+import { applicationSignin } from "@/actions/signin";
+
+interface SignInFormData {
+  email: string;
+}
+
+export function SignIn() {
+  const { control, handleSubmit } = useForm<SignInFormData>();
+  return (
+    <form
+      className="rounded-md bg-white p-6 shadow-md"
+      //   action={async (formData) => {
+      //     "use server";
+      //     await signIn("sendgrid", formData, {
+      //       redirectTo: "/application",
+      //     });
+      //   }}
+      action={applicationSignin}
+    >
+      <div className="mb-4 flex justify-center">
+        <Mail className="h-6 w-6 text-primary" />
+      </div>
+
+      <Controller
+        name="email"
+        control={control}
+        rules={{
+          required: "Email is required",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "Invalid email address",
+          },
+        }}
+        render={({ field, fieldState: { error } }) => (
+          <>
+            <input
+              {...field}
+              type="email"
+              required
+              aria-required="true"
+              placeholder="Enter your email"
+              className={`mt-1 block w-full rounded-md border px-3 py-2 shadow-sm focus:outline-none focus:ring-primary sm:text-sm ${
+                error
+                  ? "border-red-500 focus:border-red-500 focus:ring-red-500"
+                  : "border-gray-300 focus:border-primary focus:ring-primary"
+              }`}
+            />
+            {error && (
+              <p className="mt-1 text-sm text-red-500">{error.message}</p>
+            )}
+          </>
+        )}
+      />
+      <button
+        type="submit"
+        className="w-full rounded-md bg-primary px-4 py-2 text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
