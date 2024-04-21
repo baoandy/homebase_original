8231a5a9f11a413604d5cbafc88a2de0ed376751
ajricks4 <ajricks4@gmail.com>
2024-04-20 19:13:37 -0700

    mortgage details form

--- src/app/application/mortgage-details/[cardApplicationId]/MortgageDetailsForm.tsx
+++ src/app/application/mortgage-details/[cardApplicationId]/MortgageDetailsForm.tsx
@@ -2,6 +2,20 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
+import Image from "next/image";
+import barFill from "@/app/assets/Onboarding/barFill.png";
+import barNoFill from "@/app/assets/Onboarding/barNoFill.png";
+
+const styles = {
+  bar: {
+    width: "50px", // Default width for non-mobile devices
+    height: "3px",
+  },
+  barMobile: {
+    width: "15px", // Width for mobile devices
+    height: "3px",
+  },
+};
 
 interface MortgageDetailsFormData {
   cardApplicationId: string;
@@ -67,97 +81,144 @@ export default function MortgageDetailsForm({
     fetchApplication();
   }, [apiSecretKey, cardApplicationId, setValue]);
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-5">
-        <Controller
-          name="monthlyMortgagePayment"
-          control={control}
-          rules={{
-            required: "Monthly Mortgage Payment is required",
-            pattern: {
-              value: /^(?!0\d)\d*$/, // regex ensures no leading zeros and only digits
-              message:
-                "Invalid monthly mortgage amount - please enter a positive number without leading zeros.",
-            },
-          }}
-          render={({ field, fieldState: { error } }) => (
-            <>
-              <label
-                htmlFor="monthlyMortgagePayment"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Monthly Mortgage Payment
-              </label>
-              <input
-                {...field}
-                type="number"
-                id="monthlyMortgagePayment"
-                placeholder="3000"
-                min="1"
-                step="1"
-                className={`mt-1 block w-full rounded-md border px-3 py-2 shadow-sm focus:outline-none focus:ring-primary sm:text-sm ${
-                  error
-                    ? "border-red-500 focus:border-red-500 focus:ring-red-500"
-                    : "border-gray-300 focus:border-primary focus:ring-primary"
-                }`}
-              />
-              {error && (
-                <p className="mt-1 text-sm text-red-500">{error.message}</p>
+    <div className="flex w-full flex-col items-center">
+      <h1 className="text-5xl font-bold capitalize leading-[62.4px] text-zinc-800 max-md:max-w-full max-md:text-4xl">
+        <span className="text-slate-600">Mortgage</span> Details
+      </h1>
+      <p className="my-5 text-base leading-6 text-zinc-800 max-md:max-w-full">
+        Please provide the following details about your mortgage.
+      </p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-5">
+          <Controller
+            name="monthlyMortgagePayment"
+            control={control}
+            rules={{
+              required: "Monthly Mortgage Payment is required",
+              pattern: {
+                value: /^(?!0\d)\d*$/, // regex ensures no leading zeros and only digits
+                message:
+                  "Invalid monthly mortgage amount - please enter a positive number without leading zeros.",
+              },
+            }}
+            render={({ field, fieldState: { error } }) => (
+              <>
+                <label
+                  htmlFor="monthlyMortgagePayment"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Monthly Mortgage Payment
+                </label>
+                <input
+                  {...field}
+                  type="number"
+                  id="monthlyMortgagePayment"
+                  placeholder="3000"
+                  min="1"
+                  step="1"
+                  className={`mt-1 block w-full rounded-md border px-3 py-2 shadow-sm focus:outline-none focus:ring-primary sm:text-sm ${
+                    error
+                      ? "border-red-500 focus:border-red-500 focus:ring-red-500"
+                      : "border-gray-300 focus:border-primary focus:ring-primary"
+                  }`}
+                />
+                {error && (
+                  <p className="mt-1 text-sm text-red-500">{error.message}</p>
+                )}
+              </>
+            )}
+          />
+        </div>
+        <div className="mb-5">
+          <Controller
+            name="yearMortgageOriginated"
+            control={control}
+            rules={{
+              required: "The year you got your mortgage is required",
+              pattern: {
+                value: /^(?!0\d)\d*$/, // regex ensures no leading zeros and only digits
+                message:
+                  "Invalid year - please enter a positive number without leading zeros.",
+              },
+            }}
+            render={({ field, fieldState: { error } }) => (
+              <>
+                <label
+                  htmlFor="yearMortgageOriginated"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Year Mortgage Originated
+                </label>
+                <input
+                  {...field}
+                  type="number"
+                  id="yearMortgageOriginated"
+                  placeholder="2020"
+                  min="1"
+                  step="1"
+                  className={`mt-1 block w-full rounded-md border px-3 py-2 shadow-sm focus:outline-none focus:ring-primary sm:text-sm ${
+                    error
+                      ? "border-red-500 focus:border-red-500 focus:ring-red-500"
+                      : "border-gray-300 focus:border-primary focus:ring-primary"
+                  }`}
+                />
+                {error && (
+                  <p className="mt-1 text-sm text-red-500">{error.message}</p>
+                )}
+              </>
+            )}
+          />
+        </div>
+        <div className="mt-32 flex  w-full flex-col gap-5 max-md:mt-10 max-md:max-w-full max-md:flex-wrap">
+          <div className="flex justify-between gap-5 whitespace-nowrap text-base font-semibold leading-6">
+            <button
+              type="submit"
+              className="w-full rounded-md bg-primary px-4 py-2 text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
+            >
+              {loading ? (
+                <span className="loading loading-spinner loading-sm"></span>
+              ) : (
+                "Submit"
               )}
-            </>
-          )}
-        />
-      </div>
-      <div className="mb-5">
-        <Controller
-          name="yearMortgageOriginated"
-          control={control}
-          rules={{
-            required: "The year you got your mortgage is required",
-            pattern: {
-              value: /^(?!0\d)\d*$/, // regex ensures no leading zeros and only digits
-              message:
-                "Invalid year - please enter a positive number without leading zeros.",
-            },
-          }}
-          render={({ field, fieldState: { error } }) => (
-            <>
-              <label
-                htmlFor="yearMortgageOriginated"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Year Mortgage Originated
-              </label>
-              <input
-                {...field}
-                type="number"
-                id="yearMortgageOriginated"
-                placeholder="2020"
-                min="1"
-                step="1"
-                className={`mt-1 block w-full rounded-md border px-3 py-2 shadow-sm focus:outline-none focus:ring-primary sm:text-sm ${
-                  error
-                    ? "border-red-500 focus:border-red-500 focus:ring-red-500"
-                    : "border-gray-300 focus:border-primary focus:ring-primary"
-                }`}
+            </button>
+          </div>
+          <div className="mt-2 flex w-fit shrink-0 grow basis-0 flex-col self-start">
+            <div className="text-lg font-medium leading-7 text-black">5/6</div>
+            <div className="mt-2.5 flex gap-2 p-1.5">
+              <Image
+                src={barFill}
+                alt="Progress bar"
+                style={window.innerWidth < 640 ? styles.barMobile : styles.bar}
               />
-              {error && (
-                <p className="mt-1 text-sm text-red-500">{error.message}</p>
-              )}
-            </>
-          )}
-        />
-      </div>
-      <button
-        type="submit"
-        className="w-full rounded-md bg-primary px-4 py-2 text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
-      >
-        {loading ? (
-          <span className="loading loading-spinner loading-sm"></span>
-        ) : (
-          "Submit"
-        )}
-      </button>
-    </form>
+              <Image
+                src={barFill}
+                alt="Progress bar"
+                style={window.innerWidth < 640 ? styles.barMobile : styles.bar}
+              />
+              <Image
+                src={barFill}
+                alt="Progress bar"
+                style={window.innerWidth < 640 ? styles.barMobile : styles.bar}
+              />
+              <Image
+                src={barFill}
+                alt="Progress bar"
+                style={window.innerWidth < 640 ? styles.barMobile : styles.bar}
+              />
+              <Image
+                src={barFill}
+                alt="Progress bar"
+                style={window.innerWidth < 640 ? styles.barMobile : styles.bar}
+              />
+              <Image
+                src={barNoFill}
+                alt="Progress bar"
+                style={window.innerWidth < 640 ? styles.barMobile : styles.bar}
+              />
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
--- src/app/application/mortgage-details/[cardApplicationId]/page.tsx
+++ src/app/application/mortgage-details/[cardApplicationId]/page.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 
 import { env } from "@/lib/env";
 import MortgageDetailsForm from "./MortgageDetailsForm";
+import Image from "next/image";
+import homeCleaners from "@/app/assets/Onboarding/homeCleaners.png";
 
 export default async function MortgageDetails({
   params,
@@ -31,12 +33,31 @@ export default async function MortgageDetails({
   }
 
   return (
-    <div className="mx-auto max-w-md p-4">
-      <h1 className="mb-4 text-lg font-semibold">Mortgage Details</h1>
-      <MortgageDetailsForm
-        cardApplicationId={cardApplicationId}
-        apiSecretKey={env.API_SECRET_KEY}
-      />
-    </div>
+    // <div className="mx-auto max-w-md p-4">
+    //   <h1 className="mb-4 text-lg font-semibold">Mortgage Details</h1>
+    //   <MortgageDetailsForm
+    //     cardApplicationId={cardApplicationId}
+    //     apiSecretKey={env.API_SECRET_KEY}
+    //   />
+    // </div>
+    <main className="mt-20 w-full max-w-[1360px] self-center max-md:mt-10 max-md:max-w-full">
+      <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+        <div className="flex w-[58%] flex-col max-md:ml-0 max-md:w-full">
+          <Image
+            src={homeCleaners}
+            alt="Decorative image"
+            className="aspect-square w-full max-md:mt-10 max-md:max-w-full"
+          />
+        </div>
+        <div className="ml-5 flex w-[42%] flex-col max-md:ml-0 max-md:w-full">
+          <section className="mt-24 flex grow flex-col px-5 max-md:mt-10 max-md:max-w-full">
+            <MortgageDetailsForm
+              cardApplicationId={cardApplicationId}
+              apiSecretKey={env.API_SECRET_KEY}
+            />
+          </section>
+        </div>
+      </div>
+    </main>
   );
 }
