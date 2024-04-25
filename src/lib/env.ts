9b9cf3ab8c108bc6913e7c158a4981f1e05d75ff
ajricks4 <ajricks4@gmail.com>
2024-04-25 09:02:43 -0700

    add back in keys

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,12 +6,12 @@ const envSchema = zod.object({
   API_SECRET_KEY: zod.string().min(1),
   SENDGRID_API_KEY: zod.string().min(1),
 
-  // TREASURYPRIME_API_BASE_URL: zod.string().min(1),
-  // TREASURYPRIME_KEY_ID: zod.string().min(1),
-  // TREASURYPRIME_KEY_SECRET: zod.string().min(1),
+  TREASURYPRIME_API_BASE_URL: zod.string().min(1),
+  TREASURYPRIME_KEY_ID: zod.string().min(1),
+  TREASURYPRIME_KEY_SECRET: zod.string().min(1),
 
   GOOGLE_PLACES_API_KEY: zod.string().min(1),
-  // SSID_SECRET: zod.string().min(1),
+  SSID_SECRET: zod.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
