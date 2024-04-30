8fccf68b7ef85a8458bdaa993b3f3312863ba89c
ajricks4 <ajricks4@gmail.com>
2024-04-30 11:19:03 -0700

    login logo

--- src/auth.ts
+++ src/auth.ts
@@ -7,6 +7,10 @@ import EmailProvider from "next-auth/providers/email";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme: {
+    logo: "/logo.png",
+    brandColor: "#366871",
+  },
   adapter: PrismaAdapter(prisma),
   providers: [
     Google,
